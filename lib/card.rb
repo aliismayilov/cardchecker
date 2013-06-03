01bd71bd8ce81bea27a094da9345f9ea0e9a4c8f
Ali Ismayilov <ali@ismailov.info>
2013-06-03 22:21:53 +0500

    refactor begins_with

--- lib/card.rb
+++ lib/card.rb
@@ -3,21 +3,30 @@ class Card
     @number = number
   end
 
+  def begins_with?(digit_array)
+    digit_array.each do |digits|
+      if @number[0, digits.length] == digits
+        return true
+      end
+    end
+    false
+  end
+
   def type
     # begins with 34 or 37 and the length is 15
-    if @number[0,2] == '34' or @number[0,2] == '37' and @number.length == 15
+    if begins_with?(%w{34 37}) and @number.length == 15
       return 'AMEX'
 
     # begins with 6011 and the length is 16
-    elsif @number[0,4] == '6011' and @number.length == 16
+    elsif begins_with?(%w{6011}) and @number.length == 16
       return 'Discover'
 
     # begins with 51 or 55 and the length is 16
-    elsif @number[0,2] == '51' or @number[0,2] == '55' and @number.length == 16
+    elsif begins_with?(%w{51 55}) and @number.length == 16
       return 'MasterCard'
 
     # begins with 4 and the length is 13 or 16
-    elsif @number[0,1] == '4' and (@number.length == 13 or @number.length == 16)
+    elsif begins_with?(%w{4}) and (@number.length == 13 or @number.length == 16)
       return 'VISA'
 
     else

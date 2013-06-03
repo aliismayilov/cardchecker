8b07d2f3cac384e4890a87a933b5ebceec74f367
Ali Ismayilov <ali@ismailov.info>
2013-06-03 23:34:23 +0500

    format input output list

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -56,14 +56,14 @@ describe Card do
   
   describe "#output" do
     [
-      ['4111111111111111', 'VISA: 4111111111111111       (valid)'],
-      ['4111111111111', 'VISA: 4111111111111          (invalid)'],
-      ['4012888888881881', 'VISA: 4012888888881881       (valid)'],
-      ['378282246310005', 'AMEX: 378282246310005        (invalid)'],
-      ['6011111111111117', 'Discover: 6011111111111117   (valid)'],
-      ['5105105105105100', 'MasterCard: 5105105105105100 (valid)'],
-      ['5105105105105106', 'MasterCard: 5105105105105106 (invalid)'],
-      ['9111111111111111', 'Unknown: 9111111111111111    (invalid)']
+      ['4111111111111111',  'VISA: 4111111111111111       (valid)'],
+      ['4111111111111',     'VISA: 4111111111111          (invalid)'],
+      ['4012888888881881',  'VISA: 4012888888881881       (valid)'],
+      ['378282246310005',   'AMEX: 378282246310005        (invalid)'],
+      ['6011111111111117',  'Discover: 6011111111111117   (valid)'],
+      ['5105105105105100',  'MasterCard: 5105105105105100 (valid)'],
+      ['5105105105105106',  'MasterCard: 5105105105105106 (invalid)'],
+      ['9111111111111111',  'Unknown: 9111111111111111    (invalid)']
     ].each do |number, result|
       it "outputs formatted result of sanity check over card number" do
         card = Card.new(number)

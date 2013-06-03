6538c6fdd97fe3e5ffbaca365bf8a7faf65cd63a
Ali Ismayilov <ali@ismailov.info>
2013-06-03 23:28:24 +0500

    organize spec tests

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -29,57 +29,46 @@ describe Card do
   end
 
   describe "#luhn_valid?" do
-    it "returns true when submitted number is validated by Luhn algorithm" do
-      card = Card.new('4111111111111111')
-      card.luhn_valid?.should be_true
-
-      card = Card.new('4012888888881881')
-      card.luhn_valid?.should be_true
-
-      card = Card.new('6011111111111117')
-      card.luhn_valid?.should be_true
-
-      card = Card.new('5105105105105100')
-      card.luhn_valid?.should be_true
+    %w{
+      4111111111111111
+      4012888888881881
+      6011111111111117
+      5105105105105100
+    }.each do |number|
+      it "returns true when submitted number is validated by Luhn algorithm" do
+        card = Card.new(number)
+        card.luhn_valid?.should be_true
+      end
     end
 
-    it "returns false when submitted number is validated by Luhn algorithm" do
-      card = Card.new('4111111111111')
-      card.luhn_valid?.should be_false
-
-      card = Card.new('5105105105105106')
-      card.luhn_valid?.should be_false
-
-      card = Card.new('378282246310005')
-      card.luhn_valid?.should be_false
+    %w{
+      4111111111111
+      5105105105105106
+      378282246310005
+    }.each do |number|
+      it "returns false when submitted number is validated by Luhn algorithm" do
+        card = Card.new(number)
+        card.luhn_valid?.should be_false
+      end
     end
   end
 
+  
   describe "#output" do
-    it "outputs formatted result of sanity check over card number" do
-      card = Card.new('4111111111111111')
-      card.output.should eql('VISA: 4111111111111111       (valid)')
-
-      card = Card.new('4111111111111')
-      card.output.should eql('VISA: 4111111111111          (invalid)')
-
-      card = Card.new('4012888888881881')
-      card.output.should eql('VISA: 4012888888881881       (valid)')
-
-      card = Card.new('378282246310005')
-      card.output.should eql('AMEX: 378282246310005        (invalid)')
-
-      card = Card.new('6011111111111117')
-      card.output.should eql('Discover: 6011111111111117   (valid)')
-
-      card = Card.new('5105105105105100')
-      card.output.should eql('MasterCard: 5105105105105100 (valid)')
-
-      card = Card.new('5105105105105106')
-      card.output.should eql('MasterCard: 5105105105105106 (invalid)')
-
-      card = Card.new('9111111111111111')
-      card.output.should eql('Unknown: 9111111111111111    (invalid)')
+    [
+      ['4111111111111111', 'VISA: 4111111111111111       (valid)'],
+      ['4111111111111', 'VISA: 4111111111111          (invalid)'],
+      ['4012888888881881', 'VISA: 4012888888881881       (valid)'],
+      ['378282246310005', 'AMEX: 378282246310005        (invalid)'],
+      ['6011111111111117', 'Discover: 6011111111111117   (valid)'],
+      ['5105105105105100', 'MasterCard: 5105105105105100 (valid)'],
+      ['5105105105105106', 'MasterCard: 5105105105105106 (invalid)'],
+      ['9111111111111111', 'Unknown: 9111111111111111    (invalid)']
+    ].each do |number, result|
+      it "outputs formatted result of sanity check over card number" do
+        card = Card.new(number)
+        card.output.should eql(result)
+      end
     end
   end
 end
